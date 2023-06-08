5d354ed9faddbc6a6bfc92cccc5605fd46aeb91a
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-08 17:23:26 +0000

    🌿: Thursday, June 8, 2023 at 8:23:26 PM Eastern European Summer Time

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,41 @@ function App() {
     <>
       <div
         className=""
-        style={({ textAlign: "center" }, { marginTop: "100px" })}
+        style={{ backgroundColor: "#E2E2E2", minHeight: "100vh" }}
       >
-        <h1>Vite + React</h1>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
+        <div
+          className=""
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "bet",
+            position: "fixed",
+            padding: ".6rem",
+            gap: ".6rem",
+            width: "100%",
+            fontWeight: "600",
+            bottom: 0,
+          }}
+        >
+          <a href="" className="bottom-nav-bar-item">
+            Home
+          </a>
+          <a href="" className="bottom-nav-bar-item">
+            Users
+          </a>
+          <a href="" className="bottom-nav-bar-item">
+            Comments
+          </a>
+        </div>
       </div>
     </>
   );
 }
 
 export default App;
+
+// #E2E2E2
+// #FFFFFF
+// #77DD77
+// #E48DFA
