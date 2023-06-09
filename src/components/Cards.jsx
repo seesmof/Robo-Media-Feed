278dfbd94726f13fddb5c5e9f9da1077714f27cf
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-09 22:27:01 +0300

    feat: add authors to posts, add like and dislike buttons
    FIX: Fix the issue with all buttons changing state on one button click

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,4 +1,10 @@
 import React, { useEffect, useState } from "react";
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiFillDislike,
+  AiOutlineDislike,
+} from "react-icons/ai";
 
 const Cards = () => {
   const [posts, setPosts] = useState([]);
@@ -46,6 +52,9 @@ const Cards = () => {
     );
   };
 
+  const [filledHeart, setFilledHeart] = useState(false);
+  const [filledDislike, setFilledDislike] = useState(false);
+
   const Card = ({ post }) => {
     return (
       <>
@@ -59,7 +68,7 @@ const Cards = () => {
             borderRadius: ".4rem",
           }}
         >
-          <h2 style={{ marginBottom: ".6rem", fontSize: "1.2rem" }}>
+          <h2 style={{ fontSize: "1.2rem" }}>
             <a
               href="#"
               style={{
@@ -70,7 +79,51 @@ const Cards = () => {
               {post.title}
             </a>
           </h2>
+          <p
+            style={{
+              fontSize: ".9rem",
+              color: "#888",
+              marginBottom: ".8rem",
+              marginTop: ".1rem",
+            }}
+          >
+            by {users[post.userId - 1].name}
+          </p>
           <p>{post.body}</p>
+
+          <div
+            className=""
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: ".4rem",
+              marginTop: ".8rem",
+            }}
+          >
+            {/* FIXME: Fix the issue with all the buttons changing state if one is changed */}
+            <button
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+                fontSize: "1.4rem",
+                cursor: "pointer",
+              }}
+              onClick={() => setFilledHeart(!filledHeart)}
+            >
+              {filledHeart ? <AiFillHeart /> : <AiOutlineHeart />}
+            </button>
+            <button
+              style={{
+                backgroundColor: "transparent",
+                border: "none",
+                fontSize: "1.4rem",
+                cursor: "pointer",
+              }}
+              onClick={() => setFilledDislike(!filledDislike)}
+            >
+              {filledDislike ? <AiFillDislike /> : <AiOutlineDislike />}
+            </button>
+          </div>
 
           {/* TODO: Add a random amount of comments to the card, from 2 to 5. Add a like and dislike buttons. Add a show more comments button at the bottom as well as an input box for adding a new comment. */}
         </div>
