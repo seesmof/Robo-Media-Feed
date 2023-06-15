745ed8aa49e5cec4703c4b6087dbce78e78db0d5
Oleh Onyshchenko <seesmwork@gmail.com>
2023-06-15 17:23:25 +0300

    refactor: rework whole app, redesign color scheme, write functions for fetching data from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,68 @@
-import { useState } from "react";
-import Cards from "./components/Cards";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  const fetchPosts = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await res.json();
+      setPosts(data);
+      setPosts(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchUsers = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await res.json();
+      setUsers(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchComments = async () => {
+    try {
+      const res = await fetch("https://jsonplaceholder.typicode.com/comments");
+      const data = await res.json();
+      setComments(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+    fetchUsers();
+    fetchComments();
+  }, []);
+
+  const Card = () => {
+    return (
+      <>
+        <div className="card">
+          <div className="card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+            minus iusto ducimus, officia velit illum voluptas quis aut
+            exercitationem maxime.
+          </div>
+        </div>
+      </>
+    );
+  };
   return (
     <>
-      <div></div>
+      <section className="container"></section>
     </>
   );
 }
 
 export default App;
-
-// #E2E2E2
-// #FFFFFF
-// #77DD77
-// #E48DFA
